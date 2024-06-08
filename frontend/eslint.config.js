2c5df597968901a39c1f8f3ef350c13fddd5ad44
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2024-06-08 19:23:00 +0100

    chore: revert linting to eslint 8x

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,20 +2,33 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import { fixupConfigRules } from "@eslint/compat";
 import reactHooks from "eslint-plugin-react-hooks";
+import jest from "eslint-plugin-jest";
 
 export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...fixupConfigRules(pluginReactConfig),
+  pluginReactConfig,
   {
+    files: ["**/*.{js,jsx,ts,tsx}"],
     plugins: {
       "react-hooks": reactHooks,
     },
-    languageOptions: { globals: globals.browser },
+    languageOptions: { globals: { ...globals.browser, ...globals.jest } },
+    rules: { "react/react-in-jsx-scope": "off" },
+  },
+  {
+    files: ["**/*.test.{js,jsx,ts,tsx}", "**/__tests__/**/*.{js,jsx,ts,tsx}"], // Apply to test files
+    ...jest.configs["flat/recommended"],
+    plugins: {
+      jest,
+    },
     rules: {
-      "react/react-in-jsx-scope": "off", // suppress errors for missing 'import React' in files
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+      "jest/prefer-to-have-length": "warn",
+      "jest/valid-expect": "error",
     },
   },
 ];
