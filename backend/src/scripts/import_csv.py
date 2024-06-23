94012374600265a5226aad573659e14ae06fc081
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2024-06-23 14:10:43 +0200

    refactor: add some helper functions and cleanup code a little

--- backend/src/scripts/import_csv.py
+++ backend/src/scripts/import_csv.py
@@ -17,21 +17,32 @@ from db.models.matchups import Matchup
 from db.models.special_ability import SpecialAbility
 
 
+def get_deck_id_map(db_session):
+    return {name: id for id, name in db_session.query(Deck.id, Deck.name).all()}
+
+
+def fill_na_values(df, fillna_dict):
+    return df.fillna(fillna_dict)
+
+
+def rename_columns(df, columns_dict):
+    return df.rename(columns=columns_dict)
+
+
 def insert_fighter_data(db_session, df):
-    # Rename columns
-    df = df.rename(
-        columns={
+    df = rename_columns(
+        df,
+        {
             "Fighter Name": "name",
             "Fighter Type": "fighter_type",
             "Movement": "movement",
             "Starting HP": "starting_hp",
             "Range Type": "range_type",
             "Total Fighters": "total_fighters",
-        }
+        },
     )
 
-    # Map Deck Name to Deck ID
-    deck_id_map = {name: id for id, name in db_session.query(Deck.id, Deck.name).all()}
+    deck_id_map = get_deck_id_map(db_session)
     df["deck_id"] = df["Deck Name"].map(deck_id_map)
     df = df.drop(columns=["Deck Name"])  # Drop the original Deck Name column
 
@@ -39,25 +50,18 @@ def insert_fighter_data(db_session, df):
     df["fighter_type"] = df["fighter_type"].map(lambda x: FighterType[x.upper()])
     df["range_type"] = df["range_type"].map(lambda x: RangeType[x.upper()])
 
-    # Convert to list of dictionaries
     df_dict = df.to_dict(orient="records")
-
-    # Insert into database
     db_session.execute(insert(Fighter), df_dict)
     db_session.commit()
 
 
 def insert_matchup_data(db_session, df_plays, df_winrate):
-    # Create a dictionary to map deck names to their IDs
-    deck_ids = {name: id for id, name in db_session.query(Deck.id, Deck.name).all()}
-
-    # Get fighter names from the 'category' column
+    deck_ids = get_deck_id_map(db_session)
     deck_names = df_plays["Deck Name"].tolist()
 
-    # Iterate over all combinations of fighter pairs
     for deck1_name, deck2_name in itertools.product(deck_names, repeat=2):
         if deck1_name == deck2_name:
-            continue  # Skip matchups with the same fighter
+            continue
 
         deck1_id = deck_ids[deck1_name]
         deck2_id = deck_ids[deck2_name]
@@ -74,7 +78,6 @@ def insert_matchup_data(db_session, df_plays, df_winrate):
         deck1_winrate = float(deck1_winrate)
         deck2_winrate = float(deck2_winrate)
 
-        # Check for existing matchup in either direction
         existing_matchup = (
             db_session.query(Matchup)
             .filter(
@@ -99,12 +102,8 @@ def insert_matchup_data(db_session, df_plays, df_winrate):
 
 
 def insert_card_data(db_session, df):
-    # Create a map from deck name to deck ID
-    deck_id_map = {name: id for id, name in db_session.query(Deck.id, Deck.name).all()}
+    deck_id_map = get_deck_id_map(db_session)
 
-    print(df)
-
-    # Filter relevant columns for cards
     card_columns = [
         "Deck Name",
         "Unique Attack",
@@ -123,13 +122,10 @@ def insert_card_data(db_session, df):
 
     # Fill NaN values with 0 for all columns
     df_filtered = df_filtered.fillna(0)
-
     # Manually set 'Total Scheme' back to NaN
     df_filtered["Total Scheme"] = df["Total Scheme"]
 
-    # Convert DataFrame to list of dictionaries
     filtered_deck_data = df_filtered.to_dict(orient="records")
-
     cards_to_insert = []
 
     for deck_row in filtered_deck_data:
@@ -165,17 +161,14 @@ def insert_card_data(db_session, df):
                     }
                 )
 
-    # Insert cards
     if cards_to_insert:
         db_session.execute(insert(Card), cards_to_insert)
         db_session.commit()
 
 
-def insert_deck_data(db_session, df, df_deck_stats):
-    # Merge to get all deck stats
-    df_merged = pd.merge(df, df_deck_stats, on="Deck Name", how="inner")
+def insert_deck_data(db_session, df, df_stats):
+    df_merged = pd.merge(df, df_stats, on="Deck Name", how="inner")
 
-    # Fill missing values, rename columns, and select relevant columns
     df_merged = df_merged.fillna({"Number of Plays": 0, "Win Percentage": 0.0}).rename(
         columns={
             "Deck Name": "name",
@@ -185,16 +178,12 @@ def insert_deck_data(db_session, df, df_deck_stats):
         }
     )[["name", "set", "plays", "winrate"]]
 
-    # Convert to list of dictionaries
     deck_dict = df_merged.to_dict(orient="records")
-
-    # Insert into database
     db_session.execute(insert(Deck), deck_dict)
     db_session.commit()
 
 
 def insert_special_abilities(db_session, df):
-    # Filter relevant columns
     special_abilities_columns = [
         "Deck Name",
         "Special Ability 1 Name",
@@ -207,13 +196,8 @@ def insert_special_abilities(db_session, df):
     ]
     df_filtered = df[special_abilities_columns]
 
-    # Convert DataFrame to list of dictionaries
     special_abilities_list = df_filtered.to_dict(orient="records")
-
-    # Create a map from deck name to deck ID
-    deck_id_map = {name: id for id, name in db_session.query(Deck.id, Deck.name).all()}
-
-    # Array to store individually split up special abilities
+    deck_id_map = get_deck_id_map(db_session)
     special_abilities = []
 
     for row in special_abilities_list:
@@ -240,7 +224,6 @@ def insert_special_abilities(db_session, df):
                     }
                 )
 
-    # Insert special abilities
     if special_abilities:
         db_session.execute(insert(SpecialAbility), special_abilities)
         db_session.commit()
@@ -251,7 +234,6 @@ if __name__ == "__main__":
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
 
-    # Open excel file
     xls = pd.ExcelFile(
         os.path.abspath(
             os.path.join(os.path.dirname(__file__), "..", "data", "deck-fighter.xls")
