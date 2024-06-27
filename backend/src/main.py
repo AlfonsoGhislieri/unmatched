779fa379f348b352ca268e2132eadc2866e4bb6c
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2024-06-27 12:09:55 +0200

    feat: add health checks to containers

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,6 +4,12 @@ from routes import decks, fighters, matchups
 
 app = FastAPI()
 
+
+@app.get("/health")
+async def health_check():
+    return {"status": "ok"}
+
+
 # Include routers
 app.include_router(fighters.router, prefix="/fighters", tags=["fighters"])
 app.include_router(matchups.router, prefix="/matchups", tags=["matchups"])
